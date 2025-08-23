63dd0b26ccc05ff4a82c53638a96cddc8ab97e38
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-08-23 11:49:19 +0300

    Update splash_screen_page.dart

--- mazid/lib/pages/spa/splash_screen_page.dart
+++ mazid/lib/pages/spa/splash_screen_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-
-import 'package:mazid/pages/spa/widget/M_Painter.dart';
+import 'package:mazid/pages/home/home_page.dart';
+import 'package:mazid/pages/spa/widget/LogoPrintWidget.dart';
+import 'package:mazid/pages/spa/widget/LogoTextWidget';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -27,6 +28,16 @@ class _SplashScreenState extends State<SplashScreen>
       begin: 0,
       end: 1,
     ).animate(CurvedAnimation(parent: _controller, curve: Curves.easeInOut));
+
+    // بعد انتهاء الأنيميشن → يروح على الهوم
+    _controller.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (_) => const HomePage()),
+        );
+      }
+    });
   }
 
   @override
@@ -43,28 +54,13 @@ class _SplashScreenState extends State<SplashScreen>
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            AnimatedBuilder(
-              animation: _animation,
-              builder: (context, child) {
-                return CustomPaint(
-                  painter: MPainter(progress: _animation.value),
-                  size: const Size(200, 200),
-                );
-              },
-            ),
+            /// رسم اللوجو M
+            LogoPainterWidget(animation: _animation),
+
             const SizedBox(height: 30),
-            FadeTransition(
-              opacity: _animation,
-              child: const Text(
-                "Mazid Shop",
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 28,
-                  fontWeight: FontWeight.bold,
-                  letterSpacing: 2,
-                ),
-              ),
-            ),
+
+            /// النص Mazid Shop
+            LogoTextWidget(animation: _animation),
           ],
         ),
       ),
