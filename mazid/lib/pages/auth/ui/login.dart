1259f9a6a655365da5bb4f8b9bffe06be9570a7c
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-09-19 15:58:10 +0300

    Update login.dart

--- mazid/lib/pages/auth/ui/login.dart
+++ mazid/lib/pages/auth/ui/login.dart
@@ -1,13 +1,10 @@
 // ignore_for_file: use_build_context_synchronously
-
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:mazid/core/data/admin_data.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:mazid/core/data/admin_data.dart';
 import 'package:mazid/core/cubit/auth/auth_cubit.dart';
 import 'package:mazid/core/cubit/auth/auth_state.dart';
-import 'package:mazid/pages/auth/animation/login_animation.dart';
-import 'package:mazid/pages/auth/widget/from/login_form_widget.dart';
 import 'package:mazid/pages/home/ui/home_page.dart';
 
 class LoginPage extends StatefulWidget {
@@ -17,151 +14,94 @@ class LoginPage extends StatefulWidget {
   State<LoginPage> createState() => _LoginPageState();
 }
 
-class _LoginPageState extends State<LoginPage> with TickerProviderStateMixin {
-  late final AnimationController _animController;
-  late final AnimationController _entryController;
-  late final Animation<Offset> _slideAnimation;
-  late final Animation<double> _fadeAnimation;
-
+class _LoginPageState extends State<LoginPage> {
   final emailController = TextEditingController();
   final passwordController = TextEditingController();
-  bool _obscurePassword = true;
-  bool _isLoading = false;
+  bool isLoading = false;
 
-  @override
-  void initState() {
-    super.initState();
-
-    _animController = AnimationController(
-      vsync: this,
-      duration: const Duration(milliseconds: 600),
-    );
-
-    _entryController = AnimationController(
-      vsync: this,
-      duration: const Duration(milliseconds: 1000),
-    );
-
-    _slideAnimation = Tween<Offset>(
-      begin: const Offset(0, -0.2),
-      end: Offset.zero,
-    ).animate(CurvedAnimation(parent: _entryController, curve: Curves.easeOut));
-
-    _fadeAnimation = Tween<double>(
-      begin: 0.0,
-      end: 1.0,
-    ).animate(CurvedAnimation(parent: _entryController, curve: Curves.easeIn));
-
-    _entryController.forward();
-    _checkLoginStatus();
-  }
-
-  @override
-  void dispose() {
-    _animController.dispose();
-    _entryController.dispose();
-    emailController.dispose();
-    passwordController.dispose();
-    super.dispose();
-  }
-
-  Future<void> _checkLoginStatus() async {
-    final prefs = await SharedPreferences.getInstance();
-    if (prefs.getBool('isLoggedIn') ?? false) {
-      if (!mounted) return;
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(builder: (_) => const HomePage()),
-      );
-    }
-  }
-
-  Future<void> _saveLoginStatus(String email) async {
+  Future<void> _saveData({
+    required String email,
+    required String role,
+    required String name,
+  }) async {
     final prefs = await SharedPreferences.getInstance();
     await prefs.setBool('isLoggedIn', true);
     await prefs.setString('userEmail', email);
+    await prefs.setString('role', role);
+    await prefs.setString('userName', name);
   }
 
   void _handleLogin(BuildContext context) async {
     final email = emailController.text.trim();
     final password = passwordController.text.trim();
 
-    setState(() => _isLoading = true);
-    await _animController.forward();
+    setState(() => isLoading = true);
 
-    // ✅ تحقق من الأدمن
     if (email == AdminData.email && password == AdminData.password) {
-      await _saveLoginStatus(email);
-      if (!mounted) return;
+      await _saveData(email: email, role: 'admin', name: 'Administrator');
       Navigator.pushReplacement(
         context,
-        MaterialPageRoute(builder: (_) => const HomePage()), // صفحة الأدمن
+        MaterialPageRoute(builder: (_) => const HomePage()),
       );
     } else {
-      // ✅ تسجيل دخول عادي عبر الكيوبت
       context.read<AuthCubit>().loginWithEmail(
         email: email,
         password: password,
       );
     }
+    setState(() => isLoading = false);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: GestureDetector(
-        onTap: () => FocusScope.of(context).unfocus(),
-        child: BackgroundAnimation(
-          child: SafeArea(
-            child: BlocConsumer<AuthCubit, AuthState>(
-              listener: (context, state) async {
-                _animController.reverse();
-                if (!mounted) return;
-
-                if (state is Authenticated) {
-                  await _saveLoginStatus(emailController.text.trim());
-                  Navigator.pushReplacement(
-                    context,
-                    MaterialPageRoute(builder: (_) => const HomePage()),
-                  );
-                } else if (state is AuthFailure) {
-                  FocusScope.of(context).unfocus();
-                  ScaffoldMessenger.of(
-                    context,
-                  ).showSnackBar(SnackBar(content: Text(state.message)));
-
-                  // اهتزاز الصفحة عند الخطأ
-                  _entryController.forward(from: 0.0);
-                }
-
-                setState(() => _isLoading = false);
-              },
-              builder: (context, state) {
-                return Center(
-                  child: SlideTransition(
-                    position: _slideAnimation,
-                    child: FadeTransition(
-                      opacity: _fadeAnimation,
-                      child: LoginFormWidget(
-                        emailController: emailController,
-                        passwordController: passwordController,
-                        obscurePassword: _obscurePassword,
-                        onTogglePassword: () {
-                          setState(() {
-                            _obscurePassword = !_obscurePassword;
-                          });
-                        },
-                        onLoginPressed: () => _handleLogin(context),
-                        isLoading: _isLoading,
-                        animController: _animController,
-                      ),
-                    ),
+      body: BlocConsumer<AuthCubit, AuthState>(
+        listener: (context, state) async {
+          if (state is Authenticated) {
+            await _saveData(
+              email: emailController.text.trim(),
+              role: 'user',
+              name: state.user.name,
+            );
+            Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(builder: (_) => const HomePage()),
+            );
+          } else if (state is AuthFailure) {
+            ScaffoldMessenger.of(
+              context,
+            ).showSnackBar(SnackBar(content: Text(state.message)));
+          }
+        },
+        builder: (context, state) {
+          return Center(
+            child: Padding(
+              padding: const EdgeInsets.all(20),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  TextField(
+                    controller: emailController,
+                    decoration: const InputDecoration(labelText: "Email"),
                   ),
-                );
-              },
+                  const SizedBox(height: 10),
+                  TextField(
+                    controller: passwordController,
+                    obscureText: true,
+                    decoration: const InputDecoration(labelText: "Password"),
+                  ),
+                  const SizedBox(height: 20),
+                  ElevatedButton(
+                    onPressed: isLoading ? null : () => _handleLogin(context),
+                    child: isLoading
+                        ? const CircularProgressIndicator()
+                        : const Text("Login"),
+                  ),
+                ],
+              ),
             ),
-          ),
-        ),
+          );
+        },
       ),
     );
   }
