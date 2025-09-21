97ce69680215a1cbd71bcfbdb43ab3c5ef2948dd
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-09-21 17:37:23 +0300

    Update widget_test.dart

--- mazid/test/widget_test.dart
+++ mazid/test/widget_test.dart
@@ -12,7 +12,7 @@ import 'package:mazid/app.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const mazid());
+    await tester.pumpWidget(const Mazid());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
