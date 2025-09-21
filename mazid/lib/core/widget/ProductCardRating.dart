48fbb14acb7a0a3016d0db47d2bcd3a591dad148
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-09-22 02:50:02 +0300

    Update ProductCardRating.dart

--- mazid/lib/core/widget/ProductCardRating.dart
+++ mazid/lib/core/widget/ProductCardRating.dart
@@ -1,5 +1,3 @@
-// ignore_for_file: file_names
-
 import 'package:flutter/material.dart';
 
 class ProductCardRating extends StatelessWidget {
@@ -9,15 +7,15 @@ class ProductCardRating extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisSize: MainAxisSize.min,
+    return Wrap(
+      spacing: 2,
       children: List.generate(5, (index) {
         if (index < rating.floor()) {
-          return const Icon(Icons.star, color: Colors.amber, size: 18);
+          return const Icon(Icons.star, color: Colors.amber, size: 16);
         } else if (index < rating && rating % 1 != 0) {
-          return const Icon(Icons.star_half, color: Colors.amber, size: 18);
+          return const Icon(Icons.star_half, color: Colors.amber, size: 16);
         } else {
-          return const Icon(Icons.star_border, color: Colors.amber, size: 18);
+          return const Icon(Icons.star_border, color: Colors.amber, size: 16);
         }
       }),
     );
