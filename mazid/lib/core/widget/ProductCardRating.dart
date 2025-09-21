4ab55ce9f56e9152e2dfb5ed59e401a91235a92d
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-09-21 17:56:31 +0300

    Create ProductCardRating.dart

---
+++ mazid/lib/core/widget/ProductCardRating.dart
@@ -0,0 +1,24 @@
+// ignore_for_file: file_names
+
+import 'package:flutter/material.dart';
+
+class ProductCardRating extends StatelessWidget {
+  final double rating;
+
+  const ProductCardRating({super.key, required this.rating});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: List.generate(5, (index) {
+        if (index < rating.floor()) {
+          return const Icon(Icons.star, color: Colors.amber, size: 18);
+        } else if (index < rating && rating % 1 != 0) {
+          return const Icon(Icons.star_half, color: Colors.amber, size: 18);
+        } else {
+          return const Icon(Icons.star_border, color: Colors.amber, size: 18);
+        }
+      }),
+    );
+  }
+}
