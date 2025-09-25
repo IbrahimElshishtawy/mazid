1a04e3c030abc4d10d3f038c2b21136a1dcae29c
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-09-25 16:39:24 +0300

    Update Carouse_lImages_Widget.dart

--- m_shop/lib/page/home/Details/widget/Carouse_lImages_Widget.dart
+++ m_shop/lib/page/home/Details/widget/Carouse_lImages_Widget.dart
@@ -1,7 +1,6 @@
 // ignore: file_names
 // ignore_for_file: file_names, duplicate_ignore
 
-import 'package:carousel_slider/carousel_options.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
