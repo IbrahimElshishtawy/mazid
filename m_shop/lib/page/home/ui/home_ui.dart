d3febdafb3e07e13ef072407d2b62793a3ade0e0
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-09-26 20:21:58 +0300

    Update home_ui.dart

--- m_shop/lib/page/home/ui/home_ui.dart
+++ m_shop/lib/page/home/ui/home_ui.dart
@@ -1,6 +1,11 @@
 // ignore_for_file: library_private_types_in_public_api
 
 import 'package:flutter/material.dart';
+import 'package:m_shop/page/action/home/auction_home_page.dart';
+import 'package:m_shop/page/swap/ui/swap_home.dart';
+import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
 import 'package:m_shop/core/widget/home/widget/error_view.dart';
 import 'package:m_shop/page/Notifications/NotificationsPage.dart';
 import 'package:m_shop/page/home/controller/home_controller.dart';
@@ -8,9 +13,6 @@ import 'package:m_shop/page/home/section/banner_section.dart';
 import 'package:m_shop/page/home/section/categories_section.dart';
 import 'package:m_shop/page/home/section/products_grid.dart';
 import 'package:m_shop/page/profile/ui/Profile_Page.dart';
-import 'package:m_shop/page/swap/ui/swap_home.dart';
-import 'package:provider/provider.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 class HomeUI extends StatefulWidget {
   const HomeUI({super.key});
@@ -41,20 +43,20 @@ class _HomeUIState extends State<HomeUI> {
 
   @override
   Widget build(BuildContext context) {
-    // ✅ Scaffold واحد ثابت، وكل المحتوى جواه
     return Scaffold(
       backgroundColor: Colors.black,
       body: _loading
           ? const Center(child: CircularProgressIndicator(color: Colors.orange))
           : _showTermsPage
-          ? null //const AuctionHomePage()
+          ? null
           : Consumer<HomeController>(
               builder: (context, controller, child) {
                 final pages = <Widget>[
                   NotificationsPage(),
-                  // const AuctionHomePage(),
+                  AuctionHomePage(),
                   _buildHomePage(controller, context),
-                  _buildSwapPage(controller),
+
+                  SwapHome(),
                   _buildProfilePage(controller),
                 ];
 
@@ -74,10 +76,10 @@ class _HomeUIState extends State<HomeUI> {
       );
     }
 
-    if ((controller.errorMessage).isNotEmpty) {
+    if (controller.errorMessage.isNotEmpty) {
       return ErrorView(
         message: controller.errorMessage,
-        onRetry: () => controller.init(context),
+        onRetry: controller.refresh, // إعادة تحميل المنتجات
       );
     }
 
@@ -106,24 +108,12 @@ class _HomeUIState extends State<HomeUI> {
     );
   }
 
-  /// قسم الـ Swap (بدون Scaffold)
-  Widget _buildSwapPage(HomeController controller) {
-    if (controller.isUserLoading) {
-      return const Center(
-        child: CircularProgressIndicator(color: Colors.orangeAccent),
-      );
-    }
-    return const SwapHome();
-  }
-
-  /// قسم البروفايل (بدون Scaffold)
   Widget _buildProfilePage(HomeController controller) {
     if (controller.isUserLoading) {
       return const Center(
         child: CircularProgressIndicator(color: Colors.orangeAccent),
       );
     }
-
     final userId = controller.currentUser?.id ?? "guest";
     return ProfilePage(userId: userId);
   }
